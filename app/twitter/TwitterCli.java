3a8424201ffbb5bd954aefbe967a76ff79e6574b
patrick <patrick.weiszkirchner@gmail.com>
2015-05-27 17:12:28 +0200

    commit directory

--- app/twitter/TwitterCli.java
+++ app/twitter/TwitterCli.java
@@ -4,13 +4,14 @@ package twitter;
  * Created by root on 27/05/15.
  */
 public class TwitterCli implements ITwitterClient {
+    String consumerKey = "GZ6tiy1XyB9W0P4xEJudQ";
+    String consumerSecret = "gaJDlW0vf7en46JwHAOkZsTHvtAiZ3QUd2mD1x26J9w";
+    String accessToken = "1366513208-MutXEbBMAVOwrbFmZtj1r4Ih2vcoHGHE2207002";
+    String accessTokenSecret = "RMPWOePlus3xtURWRVnv1TgrjTyK7Zk33evp4KKyA";
 
     @Override
     public void publishUuid(TwitterStatusMessage message) throws Exception {
-        String consumerKey = "GZ6tiy1XyB9W0P4xEJudQ";
-        String consumerSecret = "gaJDlW0vf7en46JwHAOkZsTHvtAiZ3QUd2mD1x26J9w";
-        String accessToken = "1366513208-MutXEbBMAVOwrbFmZtj1r4Ih2vcoHGHE2207002";
-        String accessTokenSecret = "RMPWOePlus3xtURWRVnv1TgrjTyK7Zk33evp4KKyA";
+
 
     }
 }
